b889c3e77d2a9880dfcdd6622c29981c043d25ab
DayLips <daylips312@gmail.com>
2026-06-09 01:32:55 +0300

    init

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+engine = create_engine(
+    os.getenv("DATABASE_URL"),
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    ...
+
+# Подключение моделей
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
