7228c3cadf79d25c995515d2e78fa2cdb5a0c70f
Александр <alldaylips@yandex.ru>
2026-06-09 15:14:12 +0300

    init schemas

---
+++ schemas/__init__.py
@@ -0,0 +1,9 @@
+# from .campaign_desc import
+# from .hh_demographic import
+# from .product import
+# from .transaction_data import
+# from .campaign_table import
+# from .coupon import
+# from .coupon_redempt import
+
+# __all__ = [...]
