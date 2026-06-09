4fb4076cbc714a37da6b264cba0369aa24fabd00
Александр <alldaylips@yandex.ru>
2026-06-09 15:45:26 +0300

    init repositories

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,9 +1,9 @@
-# from .campaign_desc import
-# from .hh_demographic import
-# from .product import
-# from .transaction_data import
-# from .campaign_table import
-# from .coupon import
-# from .coupon_redempt import
+from .campaign_desc import CampaignDescCreate
+from .hh_demographic import HHDemographicCreate
+from .product import ProductCreate
+from .transaction_data import TransactionDataCreate
+from .campaign_table import CampaignTableCreate
+from .coupon import CouponCreate
+from .coupon_redempt import CouponRedemptCreate
 
-# __all__ = [...]
+__all__ = ['CampaignDescCreate', 'HHDemographicCreate', 'ProductCreate', 'TransactionDataCreate', 'CampaignTableCreate', 'CouponCreate', 'CouponRedemptCreate']
