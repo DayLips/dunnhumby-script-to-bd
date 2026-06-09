6b1c4da48d8c722420a202f0a505adf709349c4e
Александр <alldaylips@yandex.ru>
2026-06-09 16:02:12 +0300

    init tools

---
+++ repositories/__init__.py
@@ -0,0 +1,10 @@
+from .campaign_desc_repository import CampaignDescRepository
+from .campaign_table_repository import CampaignTableRepository
+from .coupon_redempt_repository import CouponRedemptRepository
+from .coupon_repository import CouponRepository
+from .hh_demographic_repository import HHDemographicRepository
+from .product_repository import ProductRepository
+from .transaction_data_repository import TransactionDataRepository
+
+__all__ = ['CampaignDescRepository', 'CampaignTableRepository', 'CouponRedemptRepository', 'CouponRepository', 
+           'HHDemographicRepository', 'ProductRepository', 'TransactionDataRepository']
